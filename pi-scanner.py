f848ed20ecfe77a0e8ab5ffedd5e0ee2c4f9b966
dwiechert <dan.wiechert@gmail.com>
2015-10-17 14:29:31 -0500

    Got gspread to open an excel sheet.

--- pi-scanner.py
+++ pi-scanner.py
@@ -3,13 +3,40 @@ pi-scanner
 Barcode scanner project for the Raspberry Pi.
 """
 
-def main():
-    while True:    # Read input forever
-    	barcode = raw_input("Enter the barcode: ")
-    	if barcode == "quit":
-    	    break  # Exit the program
-    	else:
-    	    print('Barcode is %s \n' % (barcode))
+import sys, getopt
+import json
+import gspread
+from oauth2client.client import SignedJwtAssertionCredentials
+
+def main(argv):
+	oauthFile = ''
+	try:
+		opts, args = getopt.getopt(argv,"i:",["ifile="])
+	except getopt.GetoptError:
+		print 'pi-scanner.py -i <oauth file>'
+		sys.exit(2)
+			
+	for opt, arg in opts:
+		if opt in ("-i", "--ifile"):
+			oauthFile = arg		
+	
+	print 'Input file is "', oauthFile
+	
+	json_key = json.load(open(oauthFile))
+	scope = ['https://spreadsheets.google.com/feeds']
+	
+	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+	
+	gc = gspread.authorize(credentials)
+	
+	wks = gc.open("Test Sheet 1").sheet1
+	
+	while True:    # Read input forever
+		barcode = raw_input("Enter the barcode: ")
+		if barcode == "quit":
+			break  # Exit the program
+		else:
+			print('Barcode is %s \n' % (barcode))
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
