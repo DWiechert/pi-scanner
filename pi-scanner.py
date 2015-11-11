02a14d75ff6b1025bfb5798247d1bb11ff0225d0
dwiechert <dan.wiechert@gmail.com>
2015-11-10 21:06:56 -0600

    Added row/col value filtering.
    Closes #3.

--- pi-scanner.py
+++ pi-scanner.py
@@ -9,10 +9,10 @@ import gspread
 from gspread.exceptions import CellNotFound
 from oauth2client.client import SignedJwtAssertionCredentials
 
-def _check_negative(value):
+def _check_positive(value):
     ivalue = int(value)
     if ivalue < 1:
-         raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+         raise argparse.ArgumentTypeError("[%s] must be a positive integer." % value)
     return ivalue
 
 def _filterByRow(seq, row):
@@ -34,25 +34,34 @@ def main():
 	parser.add_argument('-sn', dest='sheetName', action='store', required=True, help='The name of the excel sheet.')
 	parser.add_argument('-ws', dest='worksheet', action='store', required=True, help='The name of the work sheet.')
 	search_group = parser.add_mutually_exclusive_group()
-	search_group.add_argument('-rf', dest='rowFilter', action='store', help='Row number to filter results by.', type=_check_negative)
-	search_group.add_argument('-cf', dest='colFilter', action='store', help='Column number to filter results by.', type=_check_negative)
+	search_group.add_argument('-sfr', dest='searchFilterRow', action='store', help='Row number for search filtering results.', type=_check_positive)
+	search_group.add_argument('-sfc', dest='searchFilterCol', action='store', help='Column number for search filtering results.', type=_check_positive)
+	value_group = parser.add_mutually_exclusive_group()
+	value_group.add_argument('-vfr', dest='valueFilterRow', action='store', help='Row number for value filtering results.', type=_check_positive)
+	value_group.add_argument('-vfc', dest='valueFilterCol', action='store', help='Column number for value filtering results.', type=_check_positive)
 
 	args = parser.parse_args()
 
 	oauthFile = args.oauthFile
 	sheetName = args.sheetName
 	worksheet = args.worksheet
-	rowFilter = args.rowFilter
-	colFilter = args.colFilter
+	searchFilterRow = args.searchFilterRow
+	searchFilterCol = args.searchFilterCol
+	valueFilterRow = args.valueFilterRow
+	valueFilterCol = args.valueFilterCol
 
 	print('\n========================================')
 	print('Input file is [%s].' % oauthFile)
 	print('Excel sheet name is [%s].' % sheetName)
 	print('Work sheet name is [%s].' % worksheet)
-	if rowFilter is not None:
-		print ('Filtering on row [%s].' % rowFilter)
-	if colFilter is not None:
-		print ('Filtering on column [%s].' % colFilter)
+	if searchFilterRow is not None:
+		print ('Search filtering on row [%s].' % searchFilterRow)
+	if searchFilterCol is not None:
+		print ('Search filtering on column [%s].' % searchFilterCol)
+	if valueFilterRow is not None:
+		print ('Value filtering on row [%s].' % valueFilterRow)
+	if valueFilterCol is not None:
+		print ('Value filtering on column [%s].' % valueFilterCol)
 	print('========================================\n')
 
 	# Login through oauth (#6) - http://gspread.readthedocs.org/en/latest/oauth2.html
@@ -65,11 +74,6 @@ def main():
 	gc = gspread.authorize(credentials)
 	wks = gc.open(sheetName).worksheet(worksheet)
 
-	print('Cell A1 is [%s].' % wks.acell('A1').value)
-	print('Cell A2 is [%s].' % wks.acell('A2').value)
-	print('Cell B1 is [%s].' % wks.acell('B1').value)
-	print('Cell B2 is [%s].' % wks.acell('B2').value)
-
 	while True:    # Read input forever
 		barcode = raw_input("\nEnter the barcode: ")
 		if barcode == "quit":
@@ -77,19 +81,35 @@ def main():
 
 		print('Barcode is [%s].' % barcode)
 		try:
-			if rowFilter is not None:
-				cell = _filterByRow(wks.findall(barcode), rowFilter)
-			elif colFilter is not None:
-				cell = _filterByCol(wks.findall(barcode), colFilter)
+			# Search filter by row/col/or nothing
+			if searchFilterRow is not None:
+				cell = _filterByRow(wks.findall(barcode), searchFilterRow)
+			elif searchFilterCol is not None:
+				cell = _filterByCol(wks.findall(barcode), searchFilterCol)
 			else:
 				cell = wks.find(barcode)
 			print('Barcode found at row [%s] column [%s].' % (cell.row, cell.col))
+
+			# Value filter by row/col/or nothing
+			if valueFilterRow is not None:
+				valueCell = wks.cell(valueFilterRow, cell.col)
+			elif valueFilterCol is not None:
+				valueCell = wks.cell(cell.row, valueFilterCol)
+			else:
+				valueCell = cell
+			print('Value found is [%s].' % (valueCell.value))
 		except CellNotFound:
 			wks.add_rows(1)
 			row_count = wks.row_count
-			wks.update_cell(row_count, 1, barcode)
-			cell = wks.find(barcode)
-			print('Barcode added at row [%s] column [%s].' % (cell.row, cell.col))
+			# Put the value in the value filter row
+			if valueFilterCol is not None:
+				col = valueFilterCol
+			else:
+				col = 1
+			wks.update_cell(row_count, col, barcode)
+			# TODO: Determine if we want to print out where the cell was added
+			# cell = wks.find(barcode)
+			# print('Barcode added at row [%s] column [%s].' % (cell.row, cell.col))
 
 if __name__ == "__main__":
     main()
